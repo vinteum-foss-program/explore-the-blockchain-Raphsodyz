be6e8d476f8968f94377e9550542023cac3af936
Raphsodyz <arthurvsr@gmail.com>
2024-11-20 12:02:43 -0300

    Feat(Submission): First and second questions answers.

--- submission/001.sh
+++ submission/001.sh
@@ -1,1 +1,5 @@
 # What is the hash of block 654,321?
+
+# Connection parameters added on bitcoin.conf
+#!/bin/zsh
+bitcoin-cli getblockhash 654321
--- submission/002.sh
+++ submission/002.sh
@@ -2,3 +2,11 @@
 #   address: `1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa`
 #   message: `1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa`
 #   signature: `HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM=`
+
+# Connection parameters added on bitcoin.conf
+#!/bin/zsh
+address="1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
+message="1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
+signature="HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM="
+
+bitcoin-cli verifymessage $address $signature $message
