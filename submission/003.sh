b600285eb561d1fce9b5de03076ccf0f9ff54170
Raphsodyz <arthurvsr@gmail.com>
2024-11-20 13:45:33 -0300

    Feat(Submission): Question 3 answer

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,8 @@
 # How many new outputs were created by block 123,456?
+
+# Connection parameters added on bitcoin.conf
+#!/bin/zsh
+blockhash=$(bitcoin-cli getblockhash 123456)
+blockdata=$(bitcoin-cli getblock $blockhash 2)
+
+echo $blockdata | jq '[.tx[].vout | length] | add' 
