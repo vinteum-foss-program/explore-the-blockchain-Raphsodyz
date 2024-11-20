c0757d861bf9c90c0296053bb150898b9a130a4b
Raphsodyz <arthurvsr@gmail.com>
2024-11-20 18:32:08 -0300

    Feat(submission): Question 4 answer

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,12 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+
+#!/bin/zsh
+XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
+INDEX=100
+DESCRIPTOR="tr($XPUB/$INDEX/*)"
+
+ENCODED_DESCRIPTOR=$(bitcoin-cli getdescriptorinfo "$DESCRIPTOR" | jq -r '.descriptor')
+TAPROOT_ADDRESS=$(bitcoin-cli deriveaddresses "$ENCODED_DESCRIPTOR" "[$INDEX,$INDEX]" | jq -r '.[0]')
+
+echo $TAPROOT_ADDRESS
