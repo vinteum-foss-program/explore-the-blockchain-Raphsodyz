fd10c980a1b15b943e9e06864dcf37e090707e44
Raphsodyz <arthurvsr@gmail.com>
2024-11-20 18:31:46 -0300

    Fix(submission): Removed unecessary description.

--- submission/001.sh
+++ submission/001.sh
@@ -1,5 +1,4 @@
 # What is the hash of block 654,321?
 
-# Connection parameters added on bitcoin.conf
 #!/bin/zsh
 bitcoin-cli getblockhash 654321
--- submission/003.sh
+++ submission/003.sh
@@ -1,6 +1,5 @@
 # How many new outputs were created by block 123,456?
 
-# Connection parameters added on bitcoin.conf
 #!/bin/zsh
 blockhash=$(bitcoin-cli getblockhash 123456)
 blockdata=$(bitcoin-cli getblock $blockhash 2)
