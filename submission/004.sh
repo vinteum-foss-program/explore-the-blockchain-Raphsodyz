400acface92d7614faf887313a435ecefe273784
Raphsodyz <arthurvsr@gmail.com>
2024-11-23 16:26:34 -0300

    Fix(Submission): Adjust question 004 descriptor.

--- submission/004.sh
+++ submission/004.sh
@@ -4,9 +4,7 @@
 #!/bin/zsh
 XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
 INDEX=100
-DESCRIPTOR="tr($XPUB/$INDEX/*)"
+DESCRIPTOR="tr($XPUB/*)"
 
 ENCODED_DESCRIPTOR=$(bitcoin-cli getdescriptorinfo "$DESCRIPTOR" | jq -r '.descriptor')
-TAPROOT_ADDRESS=$(bitcoin-cli deriveaddresses "$ENCODED_DESCRIPTOR" "[$INDEX,$INDEX]" | jq -r '.[0]')
-
-echo $TAPROOT_ADDRESS
+bitcoin-cli deriveaddresses "$ENCODED_DESCRIPTOR" "[$INDEX,$INDEX]" | jq -r '.[0]'
