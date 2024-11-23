348efe8506775ab72178a3290365b113e0832ae8
Raphsodyz <arthurvsr@gmail.com>
2024-11-23 11:19:31 -0300

    Feat(Submissions): Question 006 answer.

--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,15 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
+
+#!/bin/zsh
+coinbaseTxid=$(bitcoin-cli getblock $(bitcoin-cli getblockhash 256128) | jq -r '.tx[0]')
+newBlockTransactions=$(bitcoin-cli getblock $(bitcoin-cli getblockhash 257343) | jq -r '.tx[]')
+
+for txid in $newBlockTransactions; do
+  inputs=$(bitcoin-cli getrawtransaction $txid true | jq -r '.vin[].txid')
+  for input in $inputs; do
+    if [ "$input" = "$coinbaseTxid" ]; then
+      echo "$txid"
+      exit 0
+    fi
+  done
+done
